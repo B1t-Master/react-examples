0f518a1a449ff469f26ba6ca6c3dc7c35433932b
B1t-Master <isaac.njuguna@strathmore.edu>
2026-06-08 10:21:00 +0300

    Messing around with state

--- state/src/App.jsx
+++ state/src/App.jsx
@@ -5,9 +5,11 @@ const COLORS = ['pink', 'green', 'blue', 'yellow', 'purple'];
 
 function App() {
   const [backgroundColor, setBackgroundColor] = useState(COLORS[0]);
+  const [counter, setCounter] = useState(0);
 
   const onButtonClick = (color) => () => {
     setBackgroundColor(color);
+    setCounter(counter + 1);
   };
 
   return (
@@ -27,6 +29,7 @@ function App() {
           {color}
         </button>
       ))}
+      <div className="counter">Counter {counter}</div>
     </div>
   );
 }
